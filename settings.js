f9c413482105e95ea4cbf0079dae012a7c8458e6
Matakoyako12 <123723699+Matakoyako12@users.noreply.github.com>
2023-01-27 16:46:53 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,30 +4,30 @@ const chalk = require('chalk')
 
 global.botname = "Queen Anya" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
+global.author = "𝗥𝖺𝗒 𝖙𝖍e̺͆e 𝕭a̸n̸d̸i̸t̸⚜️🏴‍☠️"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
+global.waterMark = "×͜× King ✘ EXRAY™ ✓" // Your own watermark 
+global.headerText = "Anya by 𝗥𝖺𝗒 𝖙𝖍e̺͆e 𝕭a̸n̸d̸i̸t̸⚜️🏴‍☠️" // text for header of menu         
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
+global.ownername = "𝗥𝖺𝗒 𝖙𝖍e̺͆e 𝕭a̸n̸d̸i̸t̸⚜️🏴‍☠️"  // Owner name //        
+global.ownernumber = ['254791195553'] 
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
-global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
+global.continent = "AFRICA" // your continent name 
+global.region = "KENYA" // your country name
+global.state = "KIAMBU" // your state name
+global.district = "THIKA" // your district name
+global.timezone = "AFRICA/EAST AFRICA" // search on google if you don't know the timezone of your country //
+global.instagramId = "exray_the_bandit" // Your insta ID
 global.email = "example@exmaple.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
-global.premium = ['916900******','917355******']  // available soon
-global.sudo = ['916900******','917355******']  // Co - Owners //
+global.premium = ['254791195553','254796695980']  // available soon
+global.sudo = ['254796695980']  // Co - Owners //
 
 global.prefa = ['-','🥵','🔥']
 
@@ -38,7 +38,7 @@ name18 = "*Λ⃝ͷʏΛ 𓆩×͜×𓆪ꪾ*"
 
 //--------Random footer message-----------\\
 footer1 = `𓄂⍣⃝🇦𝚗𝚢𝚊ㅤꪜ⃢𝟤ㅤ𓆩×͜×𓆪ꪾ `
-footer2 = `Λ⃝ͷʏΛㅤꪜ⃢𝟤ㅤ𓆩×͜×𓆪ꪾ`
+footer2 = `𝗥𝖺𝗒 𝖙𝖍e̺͆e 𝕭a̸n̸d̸i̸t̸⚜️🏴‍☠️ㅤ𓆩×͜×𓆪ꪾ`
 
 //--------Left message "Nikal msg"--------\\
 nikalmsg1 = 'Ye galiye ye choubara, yaha dikhna na dubara 😚🖕🏻' 
